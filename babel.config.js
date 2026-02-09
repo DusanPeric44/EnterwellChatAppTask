0077d9bf2d054db53040f747d6998131f6d0123d
Dušan Perić <pericdusan6@gmail.com>
2026-02-09 02:52:54 +0100

    build: add react-native-dotenv for environment variable management

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [['module:react-native-dotenv']],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
